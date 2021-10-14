8286fa95ae549d011147045916fe153ee59fd4ed
florenciia <florchutencha@gmail.com>
2021-10-14 14:37:24 -0300

    incorporar jquery al proyecto. clase 12

--- mainContacto.js
+++ mainContacto.js
@@ -2,25 +2,37 @@
 //$('#formulario')
 
 //Agregamos un formulario con jQuery
-$("body").prepend(`<h1>Consultame!</h1>
-                    <form id="formulario">
-                    <p>Nombre</p>
-                       <input classtype="text"  >
-                    <p>Email</p>
-                        <input classtype="text"  >
-                    <p>Teléfono</p>
-                       <input classtype="number">
-                    <p>Mensaje</p>
-                       <input classtype="submit">
-                   </form>`);
-//Asociamos el evento submit al formulario
-$("#formulario").submit(function (e) {
-    //Prevenimos el comportamiento de submit 
-    e.preventDefault();
-    //Obtenemos hijos del formulario
-    let hijos = $(e.target).children();
-    //Primer input type="text"
-    console.log(hijos[0].value);
-    //Primer input type="number"
-    console.log(hijos[1].value);
-});
+$("body").prepend(`<div>
+                        <button class="btnConsultame"><h1>Consultame!</h1></button>
+                    </div>
+                    <div class="form">
+                        <form id="target">
+                        <p class="text">Nombre</p>
+                        <input class="input"type="text" placeholder="Tu nombre">
+                        <p class="text">Email</p>
+                            <input class="input"type="text" placeholder="Tu email">
+                        <p class="text">Teléfono</p>
+                            <input class="input"type="text" placeholder="Tu número">
+                        <p class="text">Cantidad de clases</p>
+                            <input class="input"type="number">
+                        <p class="text">Mensaje</p>
+                            <input class="inputTextarea" type="textarea" placeholder="Dejanos un mensaje">
+                        <br>
+                        <input class="inputSubmit" type="submit">
+                        
+                        </form>
+                    </div>`);
+
+$( "#target" ).submit(function( event ) {
+    container.innerHTML = ( `<div> <p class="submitTexto"> Se ha enviado el mensaje correctamente! <br> Pronto nos estaremos comunicando con usted. </p> </div>` );
+    event.preventDefault();
+  });
+
+
+
+  /*
+//Agreguemos <h3> con jQuery ocultos con  style="display: none;"
+$("body").prepend('<h3  style="background-color: white; margin-top: 10rem" >¡Hola Coder2!</h3>');
+//Mostramos con fadeIn() todos los <h3>
+$("h3").fadeIn('slow'); //fast/slow
+*/
